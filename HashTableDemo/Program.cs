f2b7dfa15fde53faeda8310d3bf406bafc44199b
Akash <akashmane12598@gmail.com>
2020-10-24 00:26:30 +0530

    [Akash] Add. To find Frequency of words UC1

--- HashTableDemo/Program.cs
+++ HashTableDemo/Program.cs
@@ -7,6 +7,15 @@ namespace HashTableDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To HashTable Code!");
+            MyMapNode<string, string> hashset = new MyMapNode<string, string>(5);
+            hashset.Add("0", "To");
+            hashset.Add("1", "be");
+            hashset.Add("2", "or");
+            hashset.Add("3", "not");
+            hashset.Add("4", "to");
+            hashset.Add("5", "be");
+
+            Console.WriteLine("Frequency :" + hashset.GetFrequencyOfWords("be"));
         }
     }
 }
