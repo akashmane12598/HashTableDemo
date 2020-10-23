1357b35292351767a1a344694d94117715a2bfea
Akash Mane <46684836+akashmane12598@users.noreply.github.com>
2020-10-24 00:33:36 +0530

    [Akash] Refactor. Counted frequency of words in Large paragraph

--- HashTableDemo/Program.cs
+++ HashTableDemo/Program.cs
@@ -6,16 +6,25 @@ namespace HashTableDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To HashTable Code!");
-            MyMapNode<string, string> hashset = new MyMapNode<string, string>(5);
-            hashset.Add("0", "To");
-            hashset.Add("1", "be");
-            hashset.Add("2", "or");
-            hashset.Add("3", "not");
-            hashset.Add("4", "to");
-            hashset.Add("5", "be");
-
-            Console.WriteLine("Frequency :" + hashset.GetFrequencyOfWords("be"));
+            int frequency;
+            Console.WriteLine("Welcome To Hash Table Program");
+            MyMapNode<string, int> hash = new MyMapNode<string, int>(5);
+            string paragraph = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+            string[] words = paragraph.Split(" ");
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (hash.GetFrequency(words[i]) == default)
+                {
+                    frequency = 1;
+                    hash.Add(words[i], frequency);
+                }
+                else
+                {
+                    frequency = hash.GetFrequency(words[i]);
+                    hash.SetFrequency(words[i], ++frequency);
+                }
+            }
+            Console.WriteLine("Frequency :" + hash.GetFrequency("paranoid"));
         }
     }
 }
